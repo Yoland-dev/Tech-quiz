3f98eec59fa42515fbe64ee4aea703c23a1ea7be
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-05-16 18:18:40 +0000

    dom elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const userNameInput = document.getElementById('userName');
+const categorySelect = document.getElementById('categorySelect');
+const startQuizBtn = document.getElementById('startQuizBtn');
+const toggleAdminViewBtn = document.getElementById('toggleAdminViewBtn');
+const viewLeaderboardBtn = document.getElementById('viewLeaderboardBtn');
+
