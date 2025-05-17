7bdda4618d1efbed2823dd6cb787b5bc360233b3
Lukhanyo Matshebelele <luka.matshebelele@gmail.com>
2025-05-17 23:30:21 +0000

    navigate to leaderboard

--- src/score.js
+++ src/score.js
@@ -1,19 +1,28 @@
+const playAgainBtn = document.getElementById("playAgainBtn");
+const backToHomeBtn = document.getElementById("backToHomeBtn");
+const scoreDisplay = document.getElementById("userFinalScore");
+const viewLeaderboardBtn = document.getElementById(
+  "viewLeaderboardFromScoreBtn"
+);
+
 document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
   const userName = urlParams.get("userName") || "Guest";
   const score = parseInt(urlParams.get("score")) || 0;
   const total = parseInt(urlParams.get("total")) || 0;
 
-  const scoreDisplay = document.getElementById("userFinalScore");
   scoreDisplay.textContent = `${userName}'s Score: ${score} / ${total}`;
 
-  const playAgainBtn = document.getElementById("playAgainBtn");
-  const backToHomeBtn = document.getElementById("backToHomeBtn");
   const encodedName = encodeURIComponent(userName);
 
   playAgainBtn.addEventListener("click", () => {
     window.location.href = `index.html?userName=${encodedName}`;
   });
+
+  viewLeaderboardBtn.addEventListener(
+    "click",
+    () => (window.location.href = "leaderboard.html")
+  );
 
   backToHomeBtn.addEventListener("click", () => {
     window.location.href = `index.html?userName=${encodedName}`;
